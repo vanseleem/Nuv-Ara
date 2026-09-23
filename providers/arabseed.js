333fccf0b0b1107365c57ee9b44bfe7c1fcc7113
vanseleem <vanseleem@gmail.com>
2026-09-23 10:08:35 +0000

    fix(arabseed): remove &type=series filter (ArabSeed doesn't support it)

--- providers/arabseed.js
+++ providers/arabseed.js
@@ -246,7 +246,7 @@ function searchArabSeed(title, searchType = "movies") {
     }
     function doSearch(query) {
       return __async(this, null, function* () {
-        const url = `${BASE}/?s=${encodeURIComponent(query)}${searchType === "series" ? "&type=series" : "&type=movies"}`;
+        const url = `${BASE}/?s=${encodeURIComponent(query)}${searchType === "movies" ? "&type=movies" : ""}`;
         console.log(
           `[ArabSeed] Search: ${url}`
         );
